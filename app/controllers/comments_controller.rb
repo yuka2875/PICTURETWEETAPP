463f250f633840c3bba5e65c39029108fb6a4137
yuka2875 <89343394+yuka2875@users.noreply.github.com>
2026-03-30 13:48:16 +0900

    コメント機能結合テスト

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
-    redirect_to "/tweets/#{comment.tweet.id}"  # コメントと結びつくツイートの詳細画面に遷移する
+    redirect_to "/tweets/#{comment.tweet.id}"
   end
 
   private
